2f08c9af5d34fe7839ef5956af3a718ef7012735
himai3 <h.imai.mar@gmail.com>
2022-05-03 04:20:28 +0000

    aa

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import ColorfulMessage from './components/ColorfulMessage';
+import { useState } from 'react';
 
 function App() {
-  const onClickButton = () => alert();
-
+  const onClickCountUp = () => {
+    setNum(num+1);
+  }
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1>hellow react!</h1>
       <ColorfulMessage color="blue">how are you?</ColorfulMessage>
       <ColorfulMessage color="pink">im fine!</ColorfulMessage>
-      <p><button onClick={onClickButton}>OK</button></p>
+      <p><button onClick={onClickCountUp}>CountUp</button></p>
+      <p>{num}</p>
     </>
   );
 }
