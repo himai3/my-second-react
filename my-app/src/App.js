f248d915969d001b020ef402a99b0fae263ca370
himai3 <h.imai.mar@gmail.com>
2022-05-03 03:55:33 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ColorfulMessage from './components/ColorfulMessage';
 
 function App() {
+  const onClickButton = () => alert();
+
   return (
-    <h1>hellow react!</h1>
+    <>
+      <h1>hellow react!</h1>
+      <ColorfulMessage color="blue">how are you?</ColorfulMessage>
+      <ColorfulMessage color="pink">im fine!</ColorfulMessage>
+      <p><button onClick={onClickButton}>OK</button></p>
+    </>
   );
 }
 
